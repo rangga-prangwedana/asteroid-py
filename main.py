cae07c6ace19bbb2f513ab8888fb5192ae09d21f
rangga-prangwedana <vergil1st@gmail.com>
2026-01-25 20:39:13 +0700

    fix: Connecting player.py to main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 from logger import log_state
 
 def main():
     pygame.init()
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    #print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         log_state()
@@ -17,7 +24,11 @@ def main():
                 return
 
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick() / 1000
 
 
 if __name__ == "__main__":
